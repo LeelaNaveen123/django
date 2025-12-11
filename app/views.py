87db49dca8b64d4b01fddb06cbb86d182cfd5d2c
LeelaNaveen123 <144426470+LeelaNaveen123@users.noreply.github.com>
2025-12-11 12:44:14 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,27 +1,27 @@
 import json
 import matplotlib.pyplot as plt
 from django.shortcuts import render
-import os
 from django.conf import settings
+import os
 
 def home(request):
+    # Load empdetails.json
     json_path = os.path.join(settings.BASE_DIR, "empdetails.json")
-
-    with open(json_path, "r") as f:
-        empdetails = json.load(f)
+    with open(json_path, "r") as file:
+        empdetails = json.load(file)
 
     names = list(empdetails.keys())
     values = list(empdetails.values())
 
+    # Create pie chart
     plt.figure(figsize=(6, 6))
     plt.pie(values, labels=names, autopct="%1.1f%%", startangle=90)
     plt.title("Employee Compensation")
     plt.axis("equal")
 
-    # Save piechart in static directory
-    chart_path = os.path.join(settings.BASE_DIR, "app", "static", "piechart.png")
-    plt.savefig(chart_path)
+    # Save to static folder
+    static_path = os.path.join(settings.BASE_DIR, "app", "static", "piechart.png")
+    plt.savefig(static_path)
     plt.close()
 
     return render(request, "index.html")
-
