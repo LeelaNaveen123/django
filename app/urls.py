5969d0109bdda1e2b1fe6e168b113e9c61838a3a
LeelaNaveen123 <144426470+LeelaNaveen123@users.noreply.github.com>
2025-12-11 12:45:43 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+from app import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', views.home),
+]
+
 
